9d50b2be12ccee4466beea2d5d715a248b5fc268
ginberg <g.inberg@gmail.com>
2014-08-20 13:03:43 +0200

    script and readme

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
-
+#Script containing the analysis for Course project Getting and Cleaning Data
 run_analysis <- function() {
   #1: Merge Data
-  data <- mergeData()
+  data <- readAndMergeTrainingTestData()
   
   # Read features and make the feature names better suited for R
   features = read.csv("data/UCI HAR Dataset/features.txt", sep="", header=FALSE)
@@ -9,19 +9,21 @@ run_analysis <- function() {
   features[,2] = gsub('-std', 'Std', features[,2])
   features[,2] = gsub('[-()]', '', features[,2])
   
-  #2: Get only the data on mean and std. dev.
-  colsWeWant <- grep(".*Mean.*|.*Std.*", features[,2])
-  # First reduce the features table to what we want
-  features <- features[colsWeWant,]
-  # Now add the last two columns (subject and activity)
-  colsWeWant <- c(colsWeWant, 562, 563)
-  # And remove the unwanted columns from allData
-  data <- data[,colsWeWant]
+  # Read activityLabels
+  activityLabels = read.csv("data/UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
   
-  # Add the column names (features) to allData
+  #2: Get only the data on mean and std. dev.
+  colsMeanStd <- grep(".*Mean.*|.*Std.*", features[,2])
+  # Reduce the features table
+  features <- features[colsMeanStd,]
+  # Now add the last two columns (activity and subject)
+  colsMeanStd <- c(colsMeanStd, 562, 563)
+  # And select only the cols we are intersted in
+  data <- data[,colsMeanStd]
+  
+  # Add the column names (features) to data
   colnames(data) <- c(features$V2, "Activity", "Subject")
   colnames(data) <- tolower(colnames(data))
-  activityLabels = read.csv("data/UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
   currentActivity = 1
   for (currentActivityLabel in activityLabels$V2) {
     data$activity <- gsub(currentActivity, currentActivityLabel, data$activity)
@@ -29,21 +31,28 @@ run_analysis <- function() {
   }
   data$activity <- as.factor(data$activity)
   data$subject <- as.factor(data$subject)
-  tidy = aggregate(data, by=list(activity = data$activity, subject=data$subject), mean)
-  # Remove the subject and activity column, since a mean of those has no use
-  tidy[,90] = NULL
-  tidy[,89] = NULL
-  write.table(tidy, "tidy.txt", sep="\t")
+  tidyDataSet = aggregate(data, by=list(activity = data$activity, subject=data$subject), mean)
+  # Remove the activity and subject column, since a mean of those has no use
+  tidyDataSet <- tidyDataSet[,1:88]
+  write.table(tidyDataSet, "data/UCI HAR Dataset/tidy.txt", sep="\t", row.name=FALSE)
 }
 
-mergeData <- function(){
+#Read's the training and test data files and merges them
+readAndMergeTrainingTestData <- function(){
   trainingdata = read.csv("data/UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
-  trainingdata[,562] = read.csv("data/UCI HAR Dataset/train/y_train.txt", sep="", header=FALSE)
-  trainingdata[,563] = read.csv("data/UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
+  columnCount = dim(trainingdata)[2]
+  trainingdata[,columnCount+1] = read.csv("data/UCI HAR Dataset/train/y_train.txt", sep="", header=FALSE)
+  trainingdata[,columnCount+2] = read.csv("data/UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
   testingdata = read.csv("data/UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
-  testingdata[,562] = read.csv("data/UCI HAR Dataset/test/y_test.txt", sep="", header=FALSE)
-  testingdata[,563] = read.csv("data/UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
-  # Merge training and test sets
-  allData = rbind(trainingdata, testingdata)
-  allData
+  columnCount2 = dim(testingdata)[2]
+  testingdata[,columnCount2+1] = read.csv("data/UCI HAR Dataset/test/y_test.txt", sep="", header=FALSE)
+  testingdata[,columnCount2+2] = read.csv("data/UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
+  # Merge training and test sets if columnCounts are equal!
+  if (columnCount == columnCount2){
+    data = rbind(trainingdata, testingdata)
+    data
+  }
+  else{
+    print("column count from training and test does not match")
+  }
 }
